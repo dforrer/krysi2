68a3fae552de825e95daccaddf79f6af51ef2bbe
Daniel <dforrer@gmail.com>
2016-04-12 19:45:09 +0200

    added ReduceFunction

--- src/rainbow/Rainbow.java
+++ src/rainbow/Rainbow.java
@@ -1,15 +1,20 @@
 package rainbow;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Rainbow {
 
 	final static String HOLY_GRAIL = "1d56a37fb6b08aa709fe90e12ca59e12";  		// taken from Bonusaufgabe
+	final static ArrayList<String> alphabet = new ArrayList<String>(Arrays.asList("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"));
 
 	public static void main(String[] args) {
 
+		ReduceFunction rf = new ReduceFunction();
+		rf.testReduce();
+		
 		int limit = 2000;
 		RainbowTable rt = new RainbowTable(limit);
 
@@ -27,10 +32,11 @@ public class Rainbow {
 		String hashedGrail = HOLY_GRAIL;
 
 		ArrayList<ArrayList<String>> finds = new ArrayList<ArrayList<String>>();
+		String reducedGrail; 
 		for(int i = 0; i <= limit; i++) {
 
 			//	// calculate the next values
-			//	reducedGrail = reduceFunction(hashedGrail);
+			reducedGrail = rf.reduce(hashedGrail, alphabet, 7, 0);
 			//	hashedGrail = hashFunction(reducedGrail);
 
 			//	// add the finds to the array
